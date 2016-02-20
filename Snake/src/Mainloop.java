76e5d75e6ed986ce561d7b63f971fb9450a90ade
Joel Newman <JoelNew@Uvic.ca>
2016-02-20 00:33:49 -0800

    initial commit

--- Snake/src/Mainloop.java
+++ Snake/src/Mainloop.java
@@ -4,38 +4,61 @@
  * and open the template in the editor.
  */
 
+import java.awt.Point;
 /**
  *
  * @author joelnewman
  */
-import java.awt.Point;
+
 
 public class Mainloop {
     boolean dead;
     Snake henery;
+    int[][] board;
+    int totalX = 100;
+    int totalY = 100;
     Mainloop() {
         //Mainloop constructor
         System.out.println("I am in the mainloop constructor");
         henery = new Snake();
         dead = false;
+        board = new int[totalX][totalY];
     }
 
-    public Point[] pullSnakeBoard() {
+    /**
+     * This method will be used to get the information from the webserver or
+     * whatever method we get for finding out where food and other snakes are
+     */
+    public void pullSnakeBoard() {
         System.out.println("I am in pullSnakeBoard");
-        Point p1 = new Point(1, 1);
-        Point p2 = new Point(2, 2);
-        Point[] points = new Point[2];
-        points[0] = p1;
-        points[1] = p2;
-        return points;
+        for(int tempX = 0;tempX<totalX;tempX++){
+            for(int tempY = 0;tempY<totalY;tempY++){
+                // loop over the array and do some stuff
+                board[tempX][tempY] = 0;
+            }
+        }
     }
 
-    public boolean killSnake(Point[] board) {
+    /**
+     * This method will explicitly check to see if there is a snake that we can
+     * eat/kill 
+     * If there is a snake we can kill it will return True
+     * if there is not a snake we can kill it will return False
+     * 
+     * @return boolean
+     */
+    public boolean killSnake() {
         System.out.println("I can kill another snake");
         //read through board, can I kill some other snake?
         return true;
     }
 
+    /**
+     * This will retrun the location of the food 
+     * 
+     * Returns a Point object
+     * @return
+     */
     public Point locateFood() {
         System.out.println("I found food");
         // parse the grid and return a point that contains the food coordinates
@@ -43,10 +66,13 @@ public class Mainloop {
         return food;
     }
     
+    /**
+     * Main starting point that the driver calls
+     */
     public void start(){
         Point[] board = new Point[100];
         while(!dead){
-            board = pullSnakeBoard();
+            pullSnakeBoard();
             dead = true;
             
             
--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -9,10 +9,28 @@
  * @author joelnewman
  */
 import java.awt.Point;
+
 public class Snake {
+
+    /**
+     * Point object that contains the x,y coordinates of the snake head
+     */
     public Point head;
+
+    /**
+     * This is the length of the snake
+     */
     public int length;
+
+    /**
+     * This is how many turns we are from dying
+     */
     public int starvation;
+    
+    /**
+     * Empty Snake constuctor
+     * makes the head a 0,0 with a length of 3 and a starvation time of 5
+     */
     Snake(){
         //this is the main snake constructor
         head = new Point(0,0);
@@ -20,10 +38,28 @@ public class Snake {
         starvation = 5; // start with 5 turns before you die of food
     }
     
+    /**
+     * Call this method when we consume food or a snake
+     * will increase the length by 1 and reset the starvation time
+     */
     public void eat(){
         length++;
         starvation = starvation+5;
     }
+
+    /**
+     * Moves the head of the snake.
+     * newx refers to the positive or negitive movement in the x direction
+     * newy refers to the positive or negitive movement in the y direction
+     * 
+     * EX.
+     * move(1,-1) when the head was at 5,4 would result in the head being at
+     * 6,3
+     * 
+     * 
+     * @param newx
+     * @param newy
+     */
     public void move(int newx,int newy){
         head.translate(newx, newy);
     }
