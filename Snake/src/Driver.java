fc9ed74b94021bda92bb6f38864a1aa2c09c6dc0
Joel Newman <joellnewman@hotmail.com>
2016-02-20 00:34:52 -0800

    Update Driver.java

--- Snake/src/Driver.java
+++ Snake/src/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        System.out.println("I get here");
+        //System.out.println("I get here");
         Mainloop loop = new Mainloop();
         loop.start();
     }
