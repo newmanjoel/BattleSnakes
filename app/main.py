818da136b2bb375863cb703371969de500d3e215
Joel Newman <JoelNew@Uvic.ca>
2017-03-04 16:38:28 -0800

    favour empty

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import random
 
 
 class Snake(object):
-    def __init__(self,newName,row,col,unique=1,method = 'a_star',a = 0.7,b = 0.7,c = 0.1):
+    def __init__(self,newName,row,col,unique=1,method = 'a_star',a = 0.7,b = 0.7,c = 0.01):
         self.legendString = '~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQURTUVWXYZ!@#$%^&*()_+<>?:"{}|\][/.,'
         self.legendMatrix = []
         self.n = row
