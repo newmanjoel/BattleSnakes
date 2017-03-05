ff379a0eaa17a6a25dd73a1f0fc063e2acfaacf8
Joel Newman <JoelNew@Uvic.ca>
2017-03-04 17:28:18 -0800

    taunt again?

--- app/main.py
+++ app/main.py
@@ -433,10 +433,11 @@ def start():
 def move():
     data = bottle.request.json 
    
+   taunts = ['Teen Titans, Go!','“Garbage chute. Really wonderful idea. What a wonderful smell you’ve discovered!”','Snake? Snake!? SNAAAAAAAAAAAAKE!','Tis but a flesh wound…','We’re no strangers to love','You know the rules and so do I','A full commitment's what I'm thinking of','You wouldnt get this from any other guy','“Snakes. Why did it have to be snakes?”','I AM the Brute Squad']
     try:
         snek = Snake(data['game_id'],data['width'],data['height'])
         move = snek.turn(data)
-        taunt = 'his'+'s'*data['turn']
+        taunt = random.choice(taunts)
     except Exception as e:
         move = random.choice(['up','down','left','right'])
         taunt = e.message
