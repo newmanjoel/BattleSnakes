2622a151f67d48e123a59053f0fc8d0280ef9fe2
Joel Newman <JoelNew@Uvic.ca>
2019-03-02 13:44:36 -0800

    comment out the danger stuff

--- app/snake_logic.py
+++ app/snake_logic.py
@@ -149,6 +149,7 @@ class Game():
                         self.board.board.nodes[deg[0]]["Safe"] = False
                     except Exception as e:
                         logging.critical("Cant set safe mode: {}".format(e))
+                        '''
         for snake in self.board.snakes:
             if snake == self.board.ms:
                 continue
@@ -156,6 +157,7 @@ class Game():
             for node in nx.bfs_nodes(self.board.board, source=head, depth_limit=2):
                     logging.debug("adding the cost of {} edge to {}".format(False, node))
                     self.board.board.nodes[node]['Safe'] = False
+                    '''
 
     def safe_moves(self, directions, nodes):
         results = []
